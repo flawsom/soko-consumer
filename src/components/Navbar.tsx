04a71d6c56e091322958759f7ba8981678b8c3bf
Mirror83 <glenochieng045@gmail.com>
2023-11-18 17:21:28 +0300

    Reformatted with Prettier and removed unnecessary import and type annotation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,67 +1,68 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import { useParams, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 import { cn } from "~/lib/utils";
 
 class NavRoute {
-    label: string
-    href: string
-    active: boolean
+  label: string;
+  href: string;
+  active: boolean;
 
-    constructor(label: string, href: string, active: boolean = false) {
-        this.label = label
-        this.href = href
-        this.active = active
-    }
+  constructor(label: string, href: string, active = false) {
+    this.label = label;
+    this.href = href;
+    this.active = active;
+  }
 }
 
 export default function Navbar({
-    className,
-    ...props
+  className,
+  ...props
 }: React.HTMLAttributes<HTMLElement>) {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    const routes: Array<NavRoute> = [
-        new NavRoute("Categories", "/categories"),
-        new NavRoute("Cart", "/cart"),
-        new NavRoute("Purchases", "/purchases")
-    ];
+  const routes: Array<NavRoute> = [
+    new NavRoute("Categories", "/categories"),
+    new NavRoute("Cart", "/cart"),
+    new NavRoute("Purchases", "/purchases"),
+  ];
 
-    routes.forEach((route) => {
-        route.active = route.href === pathname
-    })
+  routes.forEach((route) => {
+    route.active = route.href === pathname;
+  });
 
-    return (
-        <nav
-            className={cn("flex justify-between items-center space-x-4 lg:space-x-6 py-4 px-4", className)}
-            {...props}
-        >
-            <h2 className="text-4xl">Soko</h2>
-            <ul className="flex space-x-6">
-                {routes.map((route) => (
-                    <li key={route.href}>
-                        <Link
-                            href={route.href}
-                            className={cn(
-                                "text-sm font-medium transition-colors hover:text-primary",
-                                route.active
-                                    ? "text-black dark:text-white"
-                                    : "text-muted-foreground",
-                            )}
-                        >
-                            {route.label}
-                        </Link>
-                    </li>
-
-                ))}
-                <li>
-                    <UserButton afterSignOutUrl="/" />
-                </li>
-            </ul>
-
-        </nav>
-    );
+  return (
+    <nav
+      className={cn(
+        "flex items-center justify-between space-x-4 px-4 py-4 lg:space-x-6",
+        className,
+      )}
+      {...props}
+    >
+      <h2 className="text-4xl">Soko</h2>
+      <ul className="flex space-x-6">
+        {routes.map((route) => (
+          <li key={route.href}>
+            <Link
+              href={route.href}
+              className={cn(
+                "text-sm font-medium transition-colors hover:text-primary",
+                route.active
+                  ? "text-black dark:text-white"
+                  : "text-muted-foreground",
+              )}
+            >
+              {route.label}
+            </Link>
+          </li>
+        ))}
+        <li>
+          <UserButton afterSignOutUrl="/" />
+        </li>
+      </ul>
+    </nav>
+  );
 }
